11e93326658737403d637af896812d84e39c55e9
athulharilal <75790029+athulharilal@users.noreply.github.com>
2023-06-17 10:26:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={() => setTodos([...todos, { id: Date.now(), text: todo, status: false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+  {todos.map((item) => (
+    <div className="todo" key={item.id}>
+      <div className="left">
+        <input
+          onChange={(e) => {
+            console.log(e.target.checked);
+            console.log(item);
+            setTodos((prevTodos) =>
+              prevTodos.map((todo) =>
+                todo.id === item.id ? { ...todo, status: e.target.checked } : todo
+              )
+            );
+          }}
+          value={item.status}
+          type="checkbox"
+          name=""
+          id=""
+        />
+        <p>{item.text}</p>
+      </div>
+      <div className="right">
+        <i className="fas fa-times" onClick={() => removeTodo(item.id)}></i>
+      </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 }
